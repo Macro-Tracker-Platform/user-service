be07395b1cbf2d8da6f2825fae0af3c1315e2d17
Oleg Pruh <olegpruh1@gmail.com>
2025-04-03 22:23:14 +0100

    first commit

---
+++ src/test/java/com/olehprukhnytskyi/macrotrackeruserservice/MacroTrackerUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olehprukhnytskyi.macrotrackeruserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacroTrackerUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
